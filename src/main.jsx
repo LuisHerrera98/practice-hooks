e5ee5b11bdd5cc0c7a9afe61e4e386ea05b38abb
luisherrera98 <luchoherrerafernandez@gmail.com>
2022-10-06 01:21:36 -0300

    memo practice

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import { FocusScreen } from './04-useRef/FocusScreen'
-import { Layout } from './05-useLayoutEffect/Layout'
+// import { Layout } from './05-useLayoutEffect/Layout'
+import { Memorize } from './06-memos/Memorize'
 // import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook'
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks'
 // import { SimpleForm } from './02-useEffect/SimpleForm'
@@ -12,6 +13,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-    <Layout />
+    <Memorize/>
   // </React.StrictMode>
 )
